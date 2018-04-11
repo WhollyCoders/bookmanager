18a9ae8fd12a0a313cd5f99ae53d10ebc19739cd
Michael L Parks <github@wmi-inc.com>
2018-04-11 14:26:36 -0400

    Initial Commit | Created Seed Data

---
+++ Books.js
@@ -0,0 +1,106 @@
+data = [{
+
+            "title":"The Priority Has To Be You",
+            "sub_title":"10 Keys To Achieving Optimal Health",
+            "author":["Rochelle T. Parks"],
+            "isbn":[{
+                "type":"ISBN-13",
+                "number":"9781975810627"
+            }],
+            "description":"Self-help"
+        
+        },{
+
+            "title":"The Slight Edge",
+            "sub_title":"Turning Simple Disciplines Into Massive Success",
+            "author":["Jeff Olson"],
+            "isbn":[{
+                "type":"ISBN-13",
+                "number":"9781935944867"
+            }],
+            "description":"Self-help"
+
+        },{
+
+            "title":"How To Be Rich",
+            "sub_title":"His Formulas",
+            "author":["J. Paul Getty"],
+            "isbn":[{
+                "type":"ISBN-10",
+                "number":"0515087378"
+            }],
+            "description":"Self-help"
+
+        },{
+
+            "title":"Multiple Streams Of Internet Income",
+            "sub_title":"How Ordinary People Make Extraordinary Money Online",
+            "author":["Robert G. Allen"],
+            "isbn":[{
+
+                        "type":"ISBN-10",
+                        "number":"047121888X"
+
+                    },
+                    {
+                        
+                        "type":"ISBN-13",
+                        "number":"9780471218883"
+
+                    }
+                ],
+            "description":"Self-help, Finance, Investing"
+
+        },{
+
+            "title":"Rich Dad - Poor Dad",
+            "sub_title":"What The Rich Teach Their Kids About Money- That The Poor And Middle Class Do Not!",
+            "author":["Robert T. Kiyosaki", "Sharon L. Lechter, CPA"],
+            "isbn":[{
+
+                "type":"ISBN-10",
+                "number":"0446677450"
+
+            },
+            {
+                
+                "type":"ISBN-13",
+                "number":"9780446677455"
+
+            }
+        ],
+            "description":"Self-help, Financial Education"
+
+        },{
+
+            "title":"The Seven Habits Of Highly Effective People",
+            "sub_title":"",
+            "author":["Stephen R. Covey"],
+            "isbn":[{
+                "type":"ISBN-10",
+                "number":"0671663984"
+            }],
+            "description":"Self-help"
+
+        },{
+
+            "title":"Who Moved My Cheese?",
+            "sub_title":"An Amazing Way To Deal With Change In Your Work And In Your Life",
+            "author":["Spencer Johnson, MD"],
+            "isbn":[{
+
+                "type":"ISBN-10",
+                "number":"0399144463"
+
+            },
+            {
+                
+                "type":"ISBN-13",
+                "number":"9780399144462"
+
+            }
+        ],
+            "description":"Self-help"
+
+        }
+];
